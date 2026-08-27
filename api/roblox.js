3c4c035cb1a6b12267583308726b9ebd4453af5a
Jxree <xblake195@gmail.com>
2026-08-27 19:48:35 -0400

    Update roblox.js

--- api/roblox.js
+++ api/roblox.js
@@ -1,1 +1,24 @@
+export default async function handler(req, res) {
+  var userId = req.query.userId;
+  if (!userId) {
+    return res.status(400).json({ error: 'userId required' });
+  }
 
+  try {
+    var [userRes, avatarRes] = await Promise.all([
+      fetch('https://users.roblox.com/v1/users/' + userId),
+      fetch('https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=' + userId + '&size=150x150&format=Png&isCircular=false')
+    ]);
+
+    var userData = await userRes.json();
+    var avatarData = await avatarRes.json();
+
+    res.status(200).json({
+      displayName: userData.displayName || userData.name || null,
+      username: userData.name || null,
+      avatarUrl: avatarData.data && avatarData.data[0] ? avatarData.data[0].imageUrl : null
+    });
+  } catch(e) {
+    res.status(500).json({ error: 'Failed to fetch Roblox data' });
+  }
+}
